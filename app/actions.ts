72a98f7fdcab7f94349770b388186cf21c61e02f
rabelva21 <rabelva201@gmail.com>
2026-01-05 16:53:02 +0700

    changes

--- app/actions.ts
+++ app/actions.ts
@@ -1,9 +1,38 @@
 'use server';
-import { PrismaClient } from '@prisma/client';
+
+import {PrismaClient} from '@prisma/client';
 import {revalidatePath} from 'next/cache';
 
 const prisma = new PrismaClient();
 
-export async function addTransaction(formData: FormData){
+//fungsi tambah transaksi
+export async function addTransaction(formData: FormData) {
     const text = formData.get('text');
+    const amount = formData.get('amount');
+    const type = formData.get('type');
+
+    if (!text || !amount ||!type) {
+        return;
+    }
+
+//simpan ke database 
+await prisma.transaction.create({
+    data:   {
+        text: text.toString(),
+        amount: parseFloat(amount.toString()),
+        category: type.toString() === 'INCOME' ? 'INCOME': 'EXPENSE',
+    }
+});
+
+//refresh halaman agar data baru muncul
+revalidatePath('/');
+}
+
+//fungsi hapus transaksi
+export async function deleteTransaction(id: string) {
+    await prisma.transaction.delete({
+        where: {id},
+    })
+
+    revalidatePath('/');
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,110 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+import {PrismaClient} from '@prisma/client';
+import {addTransaction, deleteTransaction} from './actions';
+import {useState} from "react";
+
+const prisma = new PrismaClient();
+
+export default async function Home(){
+  //1. Ambil data asli dari Database
+  const transactions = await prisma.transaction.findMany({
+    orderBy: { createdAt: 'desc'},
+  });
+
+  //2. Hitung Saldo
+  const amounts = transactions.map((t) =>
+  t.category === 'EXPENSE' ? -t.amount : t.amount);
+
+  const total = amounts.reduce((acc, item) => acc + item, 0);
+
+  const income = amounts
+  .filter((item) => item > 0)
+  .reduce((acc, item) => acc + item, 0);
+
+  const expense = (
+    amounts.filter((item) => item < 0). reduce ((acc, item) => acc + item, 0) * -1
   );
+
+  //3. Membuat state untuk 'text' dan 'setText'
+  const [text, setText] = useState("");
+
+  //4. Membuat fungsi 'onSubmit'
+  const onSubmit = (e: any) =>  {
+    e.preventDefault();
+    console.log("Data dikirim:", text);
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-6 bg-slate-100 font-sans text slate-800">
+    <h1 className="text"></h1>
+
+    {/* BOX SALDO */}
+    <div className="w-full max-w-md bg-white p-6 rounded-xl shadow-md mb-6">
+      <h2 className="text-sm uppercase font-bold text-slate-500"></h2>
+      <h1 className="text-3xl font-bold my-2">Rp {total.toLocaleString('id-ID')}</h1>
+
+      <div className="flex bg-white shadow-sm border mt-4 rounded-lg p-4 divide-x">
+        <div className="w-1/2 text-center">
+          <h4 className="text-sm font-semibold uppercase text-green-600">Pemasukan</h4>
+          <p className="font-bold text-lg">+Rp {income.toLocaleString('id-ID')}</p>
+        </div>
+        <div className="w-1/2 text-center">
+          <h4 className="text-sm font-semibold uppercase text-red-600">pengeluaran</h4>
+          <p className="font-bold text-lg">-Rp {expense.toLocaleString('id-ID')}</p>
+        </div>
+      </div>
+    </div>
+  
+
+    {/*FORM INPUT*/}
+    <div className="w=full max-w-md mb-8 bg-white p-6 rounded-xl shadow-sm">
+      <h3 className="text-xl font-bold border-b-2 border-slate-300 pb-2 mb-4">Tambah Transaksi</h3>
+      <form onSubmit={onSubmit} className="flex flex-col gap-3">
+       <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Keterangan. (Contoh: Gaji, Makan)"
+        className="border p-3 rounded-lg w-full focus:outline-blue-500" required/>
+      
+        <div className="flex gap-2">
+          <input
+          type="number"
+          name="amount"
+          placeholder="Nominal (Rp)..."
+          className="border p-3 rounded-lg w-full focus:outline-blue-500" required />
+          <select name="type" className="border p-3 rounded-lg bg-slate-50 cursor-pointer" >
+            <option value="EXPENSE">Pengeluaran</option>
+           <option value="INCOME">Pemasukan</option>
+          </select>
+        </div>
+        <button type="submit" className="bg-black text-white p-3 rounded-lg font-bold hover:bg-gray-800 transition mt-2">Simpan Transaksi</button>
+      </form>
+    </div>
+
+    {/* Daftar Riwayat */}
+    <div className="w-full max-w-md">
+      <h3 className="text-xl font-bold mb-4 ml-1">Riwayat Terakhir</h3>
+      {transactions.length === 0 ? (
+        <div className="text-center py-10 text-gray-400 bg-white rounded-xl border border-dashed">Belum ada transaksi</div> 
+      ) : (
+        <ul className="space-y-3">
+          {transactions.map((t) => (
+            <li key={t.id} className="flex justify-between items-center bg-white p-4 rounded-xl shadow-sm border border-slate-100">
+              <div className={`w-2 h-10 rounded-full ${t.category === 'INCOME' ? 'bg-green-500' : 'bg-red-500'}`}></div>
+              <div>
+                <p className="font-semibold text-slate-900">{t.text}</p>
+              </div>
+        <div className="flex items-center gap-4">
+          <span className={`font-bold ${t.category === 'INCOME'? 'text-green-600' : 'text-red-600'}`}>
+          {t.category === 'INCOME' ? '+' : '-'} Rp {t.amount.toLocaleString('id-ID')}</span>
+        </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+    </main>
+  )
 }
