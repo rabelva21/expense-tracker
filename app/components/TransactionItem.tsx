b5d61ed74938a0e17a23a2aba4a27b3af8be5a32
rabelva21 <rabelva201@gmail.com>
2026-01-19 03:21:36 +0700

    changes

--- app/components/TransactionItem.tsx
+++ app/components/TransactionItem.tsx
@@ -10,31 +10,38 @@ export default function TransactionItem({transaction} : {transaction: any}) {
         return num.toLocaleString('id-ID');
     };
 
+    const isIncome = transaction.amount > 0 || transaction.category === 'INCOME';
+
     return(
-        <li className="flex justify-between items-center bg-white p-4 rounded-xl shadow-sm border border-slate-100 mb-3">
-            <div className="flex items-center gap-3">
+        <li className="gorup flex justify-between items-center bg-white p-4 rounded-xl shadow-sm hover:shadow-md border border-gray-100 transition-all duration-200 hover:-translate-y-0.5">
+            <div className="flex items-center gap-4">
                 {/* garis warna penanda (hijau/merah) */}
-                <div className={`w-2 h-10 rounded-full ${
-                    transaction.category === 'INCOME' ? 'bg-green-500' : 'bg-red-500'}`}></div>
-                
+                <div className={`w-10 h-10 rounded-full flex items-center justify-center bg-opacity-10 
+                    ${isIncome ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600'}`}> 
+                <span className="font-bold text-lg">{isIncome ? '' : ''}</span>
+                </div>   
                 <div>
-                    <p className="font-semibold text-slate-900">{transaction.text}</p>
+                    <p className="font-semibold text-gray-800 text-sm md:text-base">{transaction.text}</p>
                     <p className="text-xs text-gray-400">
-                        {new Date(transaction.createdAt).toLocaleDateString()}
+                        {new Date(transaction.createdAt).toLocaleDateString('id-ID', {
+                            weekday: 'short', day: 'numeric', month: 'short'
+                        })}
                     </p>
                 </div>
             </div>
 
-            <div className="flex items-center gap-4">
-                <span className={`font-bold ${
-                transaction.category === 'INCOME' ? 'text-green-600' : 'text-red-600'}`}>
-                    {transaction.category === 'INCOME' ? '+' : '-'}Rp {formatRupiah(transaction.amount)}
+            <div className="flex items-center gap-3">
+                <span className={`font-bold tabular-nums tracking-tight 
+                ${isIncome ? 'text-green-600' : 'text-red-600'}`}>
+                    {isIncome ? '+' : '-'}Rp {formatRupiah(transaction.amount)}
                 </span>
 
                 {/* TOMBOL HAPUS */}
                 <button onClick={() => deleteTransaction(transaction.id)}
-                className="text-red-400 hover:text-red-700 font-bold ml-2">
-                    X
+                className="opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity bg-red-50 text-red-500 hover:bg-red-500 hover:text-white p-2 rounded-lg ml-2" aria-label="Hapus Transaksi">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" view-Box="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
                 </button>
             </div>
         </li>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import {addTransaction, deleteTransaction} from './actions';
 import {prisma} from '@/lib/prisma';
+import TransactionItem from './components/TransactionItem';
 
 
   //Fungsi format uang ke rupiah
@@ -21,53 +22,63 @@ export default async function Home(){
   //2. hitung saldo
   const amounts = transactions.map((t: any) =>t.category === 'EXPENSE' ? -t.amount : t.amount);
   const total = amounts.reduce((acc: number, item: number) => acc + item, 0);
-  const income = amounts.filter((item: number) => item<0).reduce((acc: number, item: number) => acc + item, 0);
+  const income = amounts.filter((item: number) => item>0).reduce((acc: number, item: number) => acc + item, 0);
   const expense = (amounts.filter((item: number) => item < 0).reduce((acc: number, item: number) => acc + item, 0) * -1);
 
   return(
-    <main className="min-h-screen bg-gray-50 text-gray-800 font-sans pb-20">
+    <main className="min-h-screen bg-gray-100 text-gray-800 font-sans pb-20">
       {/* Navbar */}
-      <nav className="bg-blue-600 text-white p-4 shadow-md">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-xl font-bold">Dompetku 💰</h1>
-          <div className="space-x-4">
-            <Link href="/" className="hover:text-blue-200 font-medium">Home</Link>
-            <Link href="/stats" className="hover:text-blue-200">Statistik</Link>
+      <nav className="bg-white/80 backdrop-blur-md sticky top-0 z-50 border-b border-gray-200">
+        <div className="container mx-auto mx-auto px-4  py-4 flex justify-between items-center">
+          <h1 className="text-xl font-bold font-extrabold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">Dompetku </h1>
+          <div className="space-x-6 text-sm font-mdeium">
+            <Link href="/" className="text-gray-600 hover:text-blue-600 transition">Home</Link>
+            <Link href="/stats" className="text-gray-600 hover:text-blue-600 transition">Statistik</Link>
           </div>
         </div>
       </nav>
 
-      <div className="container mx-auto px-4 mt-8 max-w-lg">
+      <div className="container mx-auto px-4 mt-8 max-w-xl">
 
         {/* Card Saldo Utama */}
-        <div className="bg-white rounded-xl shadow-lg p-6 mb-6 text-center border border-gray-100">
-          <h2 className="text-gray-500 text-sm uppercase tracking-wider font-semibold">Saldo Anda</h2>
-          <h1 className={`text-4xl font-bold mt-2 ${total < 0 ? 'text-red-500' : 'text-gray-800'}`}>
+        <div className="bg-gradient-to-br from-blue-600 to-purple-700 rounded-2xl shadow-xl pb-8 mb-8 text-white relative overflow-hidden">
+        <div className="absolute top-0 right-0 -mr-8 -mt-8 w-32 h-32 rounded-full bg-white/10 blur-2xl"></div>
+        <div className="absolute bottom-0 left-0 -ml-8 -mb-8 w-32 h-32 rounded-full bg-white/10 blur-2xl"></div>
+
+        <div className="relative z-10 text-center">
+          <h2 className="text-blue-100 text-sm uppercase tracking-widest font-medium mb-1">Saldo Anda</h2>
+          <h1 className="text-5xl font-bold mb-6 tracking-tight">
             {formatRupiah(total)}
           </h1>
         
-        <div className="w-1/2 text-center">
-          <span className="block text-xs font-bold text-gray-400 uppercase">Pemasukan</span>
-          <span className="block text-green-500 font-bold text-lg mt-1">{formatRupiah(income)}</span>
-        </div>
-        <div className="w-1/2 text-center">
-        <span className="block text-xs font-bold text-gray-400 uppercase">Pengeluaran</span>
-        <span className="block text-red-500 font-bold text-lg mt-1">{formatRupiah(expense)}</span>
+          <div className="bg-white/10 backdrop-blur-sm runded-xl p-4 flex divide-x divide-white/20 shadow-inner">
+            <div className="w-1/2 text-center px-2">
+              <span className="block text-xs text-blue-100 uppercase mb-1">Pemasukan</span>
+              <span className="block text-green-300 font-bold text-lg">{formatRupiah(income)}</span>
+            </div>
+          <div className="w-1/2 text-center">
+              <span className="block text-xs text-blue-100 uppercase mb-1">Pengeluaran</span>
+              <span className="block text-red-300 font-bold text-lg">{formatRupiah(expense)}</span>
+          </div>
+          </div>
         </div>
       </div>
 
       {/* Form Tambah Transaksi */}
-      <div className="bg-white rounded-xl shadow-md p-6 mb-8">
-      <h3 className="font-bold text-lg mb-4 border-b pb-2">Tambah Transasksi Baru</h3>
+      <div className="bg-white rounded-2xl shadow-sm border border-gray-100 p-6 mb-8">
+      <h3 className="font-bold text-gray-800 text-lg mb-5 flex items-center gap-2">
+        <span className="w-1 h-6 bg-blue-600 rounded-full"></span>
+        Transaksi Baru
+        </h3>
       <form action={addTransaction} className="space-y-4">
         <div>
-          <label className="block text-sm font-medium text-gray-600 mb-1">Keterangan</label>
-          <input type="text" name="text" placeholder="Cth: Gaji, Beli kopi..." required className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focusLring-blue-500 focus:outline-none transition"/>
+          <label className="block text-xs font-bold text-gray-500 uppercase mb-1 ml-1">Keterangan</label>
+          <input type="text" name="text" placeholder="Cth: Gaji, Beli kopi..." required className="w-full p-4 bg-gray-50 border border-gray-200 rounded-xl focus:ring-4 focus:ring-blue-500/10 focus:border-blue-500 focus:bg-white outline-none transition-all placeholder:text-gray-400"/>
         </div>
         <div>
-          <label className="block text-sm font-medium text-gray-600 mb-1"></label>
-          jumlah <span className="text-xs text-gray-400">(negatif = pengeluaran, positif = pemasukan)</span>
-          <input type="number" name="number" placeholder="Cth: -50000 atau 1000000" required className="w-full p-3 border border-gray-300 rounded-lg focus:ring-blue-500 focus:outline-none transition"></input>
+          <label className="block text-xs font-bold text-gray-500 uppercase mb-1 ml-1">Jumlah</label>
+          <span className="text-xs text-gray-400">(negatif = pengeluaran, positif = pemasukan)</span>
+          <input type="number" name="amount" placeholder="Cth: -50000 atau 1000000" required className="w-full p-4 bg-gray-50 border border-gray-200 rounded-xl focus:ring-4 focus:ring-blue-500/10 focus:border-blue-500 focus-bg-white outline-none transition-all"></input>
         </div>
         <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 rounded-lg transition shadow-md hover:shadow-lg transform hover-translate-y-0.5">Simpan Transaksi</button>
       </form>
@@ -75,33 +86,19 @@ export default async function Home(){
 
       {/* List Transaksi */}
       <div>
-        <h3 className="font-bold text-lg text-gray-700">Riwayat Transaksi</h3>
-        {transactions.length === 0? (
-          <p className="text-center text-gray-400 italic py-8">Belum ada transaksi.</p>
-        ) : (
-          <ul className="space-y-3">
-            {transactions.map((t) => (
-              <li key={t.id} className="bg-white p-4 rounded-lg shadow-sm border-l-4 border-transparent hover:shadow-md transition flex justify-between items center relative group"
-                style={{ borderLeftColor: t.amount < 0 ? '#ef4444' : '#22c55e'}}>
-                
-                {/* Tombol Delete */}
-                <form action={deleteTransaction.bind(null, t.id)} className="absolue -left-10 opacity-0 group-hover:opacity-100 transition-opacity">
-                  <button className="bg-red-500 text-white p-2 rounded-full shadow hover:bg-red-600">
-                    X
-                  </button>
-                </form>
-
-                <div>
-                  <p className="font-semibold text-gray-800">{t.text}</p>
-                  <p className="text-xs text-gray-400">{t.createdAt.toLocaleDateString('id-ID')}</p>
-                </div>
-                <span className={`font-bold ${t.amount < 0 ? 'text-red-500' : 'text-green-500'}`}></span>
-                </li>
-            ))}
-          </ul>
-        )}
+        <h3 className="font-bold text-lg text-gray-700 mb-4">Riwayat Transaksi</h3>
+      {transactions.length === 0?(
+        <p className="text-center text-gray-400 italic py-8 bg0white rounded-xl border border-dashed">Belum ada transaksi.</p>
+      ) : (
+        <ul className="space-y-3">
+          {transactions.map((t) =>(
+            <TransactionItem key={t.id} transaction={t}/>
+          ))}
+        </ul>
+      )}
       </div>
-    </div>
+
+      </div>
     </main>
   );
 }
